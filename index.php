7dabff3858f02996feafeec6197b48643fae30b3
Gianluca Mazzini <gianluca@mazzini.org>
2016-02-28 11:28:11 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<LINK REL=StyleSheet HREF="style.css" TYPE="text/css" MEDIA=screen>
+<h1>PeeringDB analysis by Gianluca Mazzini</h1>
+
+Data analysis is perfomed on peeringdb.com version 2 database;
+raw data are downloaded one time every day;
+v4capacity is computed by looking the presence of v4 address;
+v6capacity is computed by looking the presence of v6 address;
+we are not responsible for any use of data shown in this site;
+we are not responsible for the correctness, completeness or up-to-dateness of data shown in this site.
+
+<br><br>
+<a href="customers.html">Rank by customers</a><br><br>
+
+<a href="v4capacity.html">Rank by IPv4 capacity</a><br>
+<a href="v6capacity.html">Rank by IPv6 capacity</a><br><br>
+
+<a href="customers-us.html">Rank by customers only US</a><br>
+<a href="customers-gb.html">Rank by customers only GB</a><br>
+<a href="customers-de.html">Rank by customers only DE</a><br>
+<a href="customers-nl.html">Rank by customers only NL</a><br>
+<a href="customers-fr.html">Rank by customers only FR</a><br>
+<a href="customers-it.html">Rank by customers only IT</a><br>
